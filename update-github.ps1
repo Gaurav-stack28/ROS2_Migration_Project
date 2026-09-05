e06d2600944fa860168687a06bbdc74e6997a558
Gaurav More <gauravm8469@gmail.com>
2026-09-06 00:31:57 +0200

    NMPC_right_lane_follower

--- update-github.ps1
+++ update-github.ps1
@@ -1,7 +1,14 @@
+$Message = Read-Host "Enter the name/description of your change"
+
+if ([string]::IsNullOrWhiteSpace($Message)) {
+    Write-Host "No commit message entered. Cancelled."
+    exit
+}
+
 git add .
 
 if (-not (git diff --cached --quiet)) {
-    git commit -m "Update project"
+    git commit -m "$Message"
 
     if ($LASTEXITCODE -eq 0) {
         git push
